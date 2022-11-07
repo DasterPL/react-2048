e68fc9a722048366bff5f4d9fa158896d902c776
DasterPL <daster7@o2.pl>
2022-11-07 14:52:31 +0100

    🧹Some cleaning

--- src/components/GameOver.js
+++ src/components/GameOver.js
@@ -1,5 +1,5 @@
 import React from 'react'
 
-export default function GameOver({ score }) {
+export default function GameOver() {
     return <div className='game-over'>Game Over</div>
 }
--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import Game from './components/Game';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 const hueValue = Math.floor(Math.random() * 360);
-document.body.style.setProperty('--hue-value', hueValue)
+document.body.style.setProperty('--hue-value', hueValue);
 root.render(
   <React.StrictMode>
-    <Game/>
+    <Game />
   </React.StrictMode>
 );
